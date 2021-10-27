40903b43fbfe41c413f45e0f23b87e0c465cb494
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 17:25:00 +0100

    Update cpu3.sv

--- cpu4/dissapointment/3_4_1/cpu3.sv
+++ cpu4/dissapointment/3_4_1/cpu3.sv
@@ -1,25 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: CPU
-//            :
-// File name  : cpu.sv
-//            :
-// Description: Top level of basic processor
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : Mark Zwolinski
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : mz@ecs.soton.ac.uk
-//
-// Revision   : Version 1.0 05/08/08
-//            : Version 1.1 17/12/13
-//            : Version 1.2 11/12/14
-//            : Version 1.3 15/01/18
-/////////////////////////////////////////////////////////////////////
 module CPU #(parameter WORD_W = 8, OP_W = 3)
             (input logic clock, n_reset, input logic [WORD_W-1:0] sw,
             output logic [6:0] disp0, disp1, disp2, disp3);
