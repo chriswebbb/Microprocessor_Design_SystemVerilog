28a6e3235b976092dbd6f4d30e350110c782a1fa
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 16:23:12 +0100

    Update ram.sv

--- cpu2/ram.sv
+++ cpu2/ram.sv
@@ -1,31 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: RAM
-//            :
-// File name  : ram.sv
-//            :
-// Description: Synchronous RAM for basic processor
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : Mark Zwolinski
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : mz@ecs.soton.ac.uk
-//
-// Revision   : Version 1.0 05/08/08
-//            : Version 1.1 17/12/13
-//            : Version 1.2 12/12/14 Only map top half of address range
-/////////////////////////////////////////////////////////////////////
-/*
-	Modified by original source
-	Author:		Alex Mansfield
-	
-	Licence:	As stated above
-*/
-
 module RAM #(parameter WORD_W = 8, OP_W = 3)
                (input logic clock, n_reset, MDR_bus, load_MDR, load_MAR, CS, R_NW,
                 inout wire [WORD_W-1:0] sysbus);
