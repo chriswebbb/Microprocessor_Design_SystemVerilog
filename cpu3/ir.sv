e3280facfad69883c028a1179e55b39daf6e2a80
Christian Webb <36595425+chriswebbb@users.noreply.github.com>
2021-10-27 16:47:14 +0100

    Update ir.sv

--- cpu3/ir.sv
+++ cpu3/ir.sv
@@ -1,24 +1,3 @@
-/////////////////////////////////////////////////////////////////////
-// Design unit: IR
-//            :
-// File name  : ir.sv
-//            :
-// Description: Instruction register for basic processor
-//            :
-// Limitations: None
-//            : 
-// System     : SystemVerilog IEEE 1800-2005
-//            :
-// Author     : Mark Zwolinski
-//            : School of Electronics and Computer Science
-//            : University of Southampton
-//            : Southampton SO17 1BJ, UK
-//            : mz@ecs.soton.ac.uk
-//
-// Revision   : Version 1.0 05/08/08
-//            : Version 1.1 17/12/13
-/////////////////////////////////////////////////////////////////////
-
 module IR  #(parameter WORD_W = 8, OP_W = 3)
              (input logic clock, n_reset, Addr_bus, load_IR,
               inout wire [WORD_W-1:0] sysbus,
